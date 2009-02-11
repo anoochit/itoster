4b3d9c430ec91703b1530091324b69989c690be8
anoochit <anoochit@023dc95e-dec6-11dd-993c-4d20d8c3c59d>
2009-02-11 11:44:19 +0000

    add burning function

--- itoaster.py
+++ itoaster.py
@@ -28,6 +28,7 @@ class gui:
 	
 	def __init__(self):
 		
+		# FIXME: you can use sqlite to store cd data
 		# Read a file
 		file = open("distrolist.txt")
 		self.index=1
@@ -35,10 +36,16 @@ class gui:
 			# make dict for iso_file
 			line_str=line.split('|')
 			self.dictname=self.index;
-			gui.iso_file[self.dictname]=line_str[1]
-			gui.iso_title[self.dictname]=line_str[0]
+			
+			# FIXME: fix total button = 26 and label must not null
+			if (self.index < 26) and (line_str[0]!="") :
+			  gui.iso_file[self.dictname]=line_str[1]
+			  gui.iso_title[self.dictname]=line_str[0]
+			
 			self.index=self.index+1	
 		file.close()
+		
+		# show dict
 		print gui.iso_file
 		print gui.iso_title
 				
@@ -66,8 +73,16 @@ class gui:
 	def on_burn(self, widget):
 		self.name= widget.name
 		self.file_index= int((self.name.split('button')[1]))
+		
+		# print iso filename
 		print gui.iso_file[self.file_index]	
 		
+		# FIXME: I didn't check for file exist
+		self.isofile="nautilus-cd-burner --source-iso=iso/"+gui.iso_file[self.file_index]
+		print self.isofile
+		
+		# run nautilus cd burner with selected iso image
+		os.system(self.isofile)		
 		
 	
 	def on_delete_event(self, widget, event):
