31e31474f8c0985b705b1240de98de9ddfa1c04c
anoochit <anoochit@023dc95e-dec6-11dd-993c-4d20d8c3c59d>
2009-02-11 09:36:45 +0000

    add trunk

--- trunk/itoaster.py
+++ trunk/itoaster.py
@@ -6,19 +6,56 @@ import gtk.glade
 import string
 import os
 import gobject
+import pickle
+
+try:  
+	import pygtk  
+	pygtk.require("2.0")  
+except:  
+	pass  
+try:  
+	import gtk  
+	import gtk.glade  
+except:  
+	print("GTK Not Availible")
+	sys.exit(1)
+
 
 class gui:
 	
+	iso_file = {}
+	
 	def __init__(self):
+		
+		# Read a file
+		file = open("distrolist.txt")
+		self.index=1
+		for line in file.readlines():
+			line_str=line.split('|')
+			self.dictname=self.index;
+			gui.iso_file[self.dictname]=line_str[1]
+			self.index=self.index+1
+		file.close()
+		print gui.iso_file
+		
+		# Load GUI
 		self.wTree=gtk.glade.XML('itoaster.glade')
-		dic = {	"on_cancel": (gtk.main_quit) }
+		dic = {	"on_cancel": (gtk.main_quit),
+						"on_burn": self.on_burn
+					}
 		self.wTree.signal_autoconnect(dic)
 		self.count = 0
 		self.win = self.wTree.get_widget("window1")
 		self.win.connect("delete_event", self.on_delete_event)
 		self.win.maximize()
 		self.win.show()
-	
+
+	def on_burn(self, widget):
+		self.name= widget.name
+		self.file_index= int((self.name.split('button')[1]))
+		print gui.iso_file[self.file_index]
+		
+		
 	
 	def on_delete_event(self, widget, event):
 		self.win.set_sensitive(False)
