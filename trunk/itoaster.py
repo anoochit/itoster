611903a863a9951969c983785541e6bf82752298
anoochit <anoochit@023dc95e-dec6-11dd-993c-4d20d8c3c59d>
2009-02-11 06:50:01 +0000

    add mocup code in python gtk

---
+++ trunk/itoaster.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import pygtk
+import gtk
+import gtk.glade
+import string
+import os
+import gobject
+
+class gui:
+	def __init__(self):
+		self.wTree=gtk.glade.XML('itoaster.glade')
+		dic = {	}
+		self.wTree.signal_autoconnect(dic)
+		self.count = 0
+		self.win = self.wTree.get_widget("window1")
+		self.win.connect("delete_event", self.on_delete_event)
+		self.win.maximize()
+		self.win.show()
+	
+	
+	def on_delete_event(self, widget, event):
+		self.win.set_sensitive(False)
+	        dialog = gtk.MessageDialog(self.win,
+	        	gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+	        	gtk.MESSAGE_INFO, gtk.BUTTONS_YES_NO, None)
+	        dialog.set_markup('<big><b>Are you sure you want to quit?</b></big>')
+	        dialog.connect("destroy", lambda w: self.win.set_sensitive(True))
+	        answer = dialog.run()
+		if answer == -8:
+			dialog.destroy()
+			return False
+
+		if answer == -9:
+			dialog.destroy()
+			return True
+
+app = gui()
+gtk.main()
